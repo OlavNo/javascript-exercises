d2f1e104d273190f1899b0539200874864a247f3
Olav Nordvik <olavnordv@gmail.com>
2024-07-22 01:24:16 +0200

    finished ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/\s/g, ''); // remove spaces
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""); // remove punctuation
+    str = str.toLowerCase(); 
 
+    var isPalindrome = true;
+    for (var i = 0; i < str.length/2; i++) {
+        if (str.charAt(i) !== str.charAt(str.length - (i + 1))) {
+            console.log("first " + str.charAt(i) + "\nlast " + str.charAt(str.length - (i + 1)));
+            isPalindrome = false;
+            break;
+        }
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
