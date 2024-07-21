647134297d02353a4263b5ae0584ffcf22cb0a10
Olav Nordvik <olavnordv@gmail.com>
2024-07-22 00:02:06 +0200

    finished ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  var sum = 0;
+	for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  var mul = 1;
+	for (var i = 0; i < array.length; i++) {
+    mul *= array[i];
+  }
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return Math.pow(num, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  var fac = 1;
+	for (var i = 1; i <= num; i++) {
+    fac *= i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
