b28e4d05d0ca2f2bc1ede1398fe121d9ea640e63
Olav Nordvik <olavnordv@gmail.com>
2024-07-22 02:05:17 +0200

    finished ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(people) {
+    
+
+    var oldestPerson = people.map((person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        return {
+            name: person.name,
+            age: person.yearOfDeath - person.yearOfBirth,
+        }
+    })
+    .reduce((current, next) => {
+        if (next.age > current.age) return next;
+        else return current;
+    });
+    console.log(oldestPerson.age);
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
